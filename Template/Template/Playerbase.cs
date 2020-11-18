c516452e065c52cd2157df327546e145d19e7512
Malik950 <54435682+Malik950@users.noreply.github.com>
2020-11-18 08:17:19 +0100

    Add files via upload

---
+++ Template/Template/Playerbase.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+
+namespace Template
+{
+    class Playerbase
+    {
+        protected Texture2D texture;
+        protected Vector2 xwingpos = new Vector2(100, 100);
+
+        public virtual void Update()
+        {
+
+        }
+
+        public Playerbase(Texture2D tex)
+        {
+            texture = tex;
+        }
+
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, xwingpos, Color.White);
+        }
+
+    }
+}
---
+++ Template/Template/player.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Template
+{
+    class Player : Playerbase
+    {
+        KeyboardState oldKState;
+        List<Bullets> BulletsList;
+        Texture2D bulletTex;
+        public Player(Texture2D tex, List<Bullets> bulletList, Texture2D bulleTex) : base(tex)
+        {
+            bulletTex = bulleTex;
+            BulletsList = bulletList;
+        }
+        public override void Update()
+        {
+            KeyboardState kstate = Keyboard.GetState();
+            if (kstate.IsKeyDown(Keys.Right))
+                xwingpos.X += 10;
+            if (kstate.IsKeyDown(Keys.Left))
+                xwingpos.X -= 10;
+            if (kstate.IsKeyDown(Keys.Up))
+                xwingpos.Y -= 10;
+            if (kstate.IsKeyDown(Keys.Down))
+                xwingpos.Y += 10;
+            if (kstate.IsKeyDown(Keys.Space) && oldKState.IsKeyUp(Keys.Space))
+            {
+                BulletsList.Add(new Bullets(bulletTex, xwingpos));
+            }
+
+
+            oldKState = kstate;
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
